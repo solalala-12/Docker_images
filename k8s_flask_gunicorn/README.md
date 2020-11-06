72a38d722e0bb5a980f48ea6654915701eab5943
sora <charmingsora12@gmail.com>
2020-11-06 09:14:00 +0900

    remake

--- k8s_flask_gunicorn/README.md
+++ k8s_flask_gunicorn/README.md
@@ -1,5 +1,75 @@
-# k8s_flask
+#  모델 소스 관리
 
-Flask Rest API Using K8s
+Rest API에 필요한 모듈과 코드는 **zip형태로** Storage(Cloud or NAS or Server)에서 관리함.  
+모델과 소스는 파일명이 같을 때 자동 덮어쓰기 함.
+모델은 YYYYMMDD.zip 으로 관리하며 소스는 model_name.zip으로 관리한다.
+ 
 
-<img src="README_.JPG">
+> ex)
+> model_name= test_model로 설정 시,
+ model path = ./test_model/model/20201104.zip  
+ sources path = ./test_model/src/test_model.zip
+
+
+
+#  1. 환경 세팅
+
+ 1. Install Docker
+ 2. Install K8s
+ 3. Docker image build 
+ 4. pod-info.yaml 환경변수 세팅 (hostPath, MODEL_NAME, MODEL_TYPE, 인증 정보 등)
+ 5. K8s 컨테이너 실행
+	 `kubectl apply -f pod-info.yaml`
+	 
+6. `python3 app.py`
+ 
+
+# 2. 모델 & 모듈 소스 다운로드
+
+ 컨테이너 최초 실행 시 모델, 모듈 소스가 로컬에 존재 해야하고 컨테이너가 실행 될 때 이전 모델 로드가 되어있는 상태.
+
+ - Source Download  [http://{serverIp}:{serverPort}/src_download](http://%7bserverIp%7d:%7bserverPort%7d/src_download)  
+- Model  Download [http://{serverIp}:{serverPort}/model_download](http://%7bserverIp%7d:%7bserverPort%7d/model_download)  
+  
+
+
+> 소스 다운로드 후 로드 되어있던 모델로 Test 진행.  
+모델 다운로드 후 변경 모델로 Test 진행.  
+
+
+##  소스 구성 
+
+ - Tensorflow
+
+	ㅡ src/
+	|ㅡㅡ requirements.txt
+	|ㅡㅡ*model_sources_files*
+	ㅡmodel/
+	|ㅡㅡ*model_checkpoint_files*
+	
+```
+class ex_model(object):
+	def __init__(self):
+			...
+			
+	def build(self): # 선택
+			...
+			
+	def load(self,model_path):
+		ckpt=tf.train.get_checkpoint_state(model_path)
+		...
+		sess = tf.Session()
+		return sess
+		
+	def predict(self, session)
+		return result
+```
+
+
+
+ - Scikit-Learn
+	ㅡ src/
+	|ㅡㅡ requirements.txt
+	|ㅡㅡmodel_sources_files
+	ㅡmodel/
+	|ㅡㅡmodel_checkpoint_files(.pkl)
--- k8s_flask_gunicorn/app.py
+++ k8s_flask_gunicorn/app.py
@@ -1,9 +1,46 @@
 import os
+
 from flask import Flask, url_for, jsonify, request, make_response,render_template
 from celery import Celery, states
 import time
-import pysftp
 # from flask_cors import CORS, cross_origin
+
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import pysftp
+import logging
+import zipfile
+import sys
+
+import json
+import numpy as np
+
+
+
+
+# 환경변수 , 
+model_type=os.environ['MODEL_TYPE']
+model_name=os.environ['MODEL_NAME']
+tf_sess=None
+today=None
+target_model=None
+
+# 변경가능
+
+sys.path.append('./'+model_name+'/src/')
+
+sys.path.append('./'+model_name+'/model/')
+
+# 로그 생성
+logger = logging.getLogger()
+# 로그의 출력 기준 설정
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# log를 파일에 출력
+file_handler = logging.FileHandler('./logs/my.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
 
 app = Flask(__name__)
 # 보안 모델
@@ -11,38 +48,278 @@ app = Flask(__name__)
 
 app.config['CELERY_BROKER_URL'] = 'amqp://rabbit_sora:dlthfk77@localhost/rabbit'
 app.config['CELERY_RESULT_BACKEND'] = 'rpc://'
+app.config['JSON_AS_ASCII']=False
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 
-@app.route('/',methods=['GET'])
+@app.route('/')
 def index():
-    x = request.args.get("model")
-    if x=='ex':
-        get_model()
-        return 'Done'
-    return 'Failed'
-        
 
-@celery.task(bind=True)
-def get_model(self):
+    return 'hi'
 
 
-    with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
-        print ("Connection succesfully stablished ... ")
-    # Define the file that you want to download from the remote directory
-        remoteFilePath = '/data01/logs/analy/maipksap01.allng.com/batch/ir_get_item_parameters_drl.log'
-        localFilePath = './test.log'
+@app.route('/post',methods=['POST']) 
+def hi():
 
-        sftp.get(remoteFilePath, localFilePath)
+    target_model = model_load()
+
+    data = request.get_json(force=True)
+
+    input_data = json.loads(data)["test"]
+
+    if model_type=='sklearn':
+
+        result = target_model.predict(input_data)
+    else:
+        global tf_sess
+        print('세션 정보 : ',tf_sess)
+        os.chdir("/root/model_rest_api/"+model_name+'/src/')
+        result = target_model.predict(tf_sess,input_data)
+
+
+    if type(result).__module__ == np.__name__:
+
+
+        result_dict={'result':result.tolist()}
+    else:
+        result_dict={'result':result}
+
+
+    resp = make_response(jsonify(result_dict))
+
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+    
     
 
 
 
+@app.route('/model_download')
+def model_download():
+
+    
+    
+    check_model_state=download(target='model')
+    target_model = model_load()
+
+    if not check_model_state:
+        return '* * * * Failed download model * * * *'
+    else:
+        return '* * * * Succeeded download model * * * * \n'+ str(target_model) 
+
+
+
+@app.route('/src_download')
+def src_download():
+    
+    check_model_state=download(target='src')
+    # os.system('pip3 install -r {}'.format(model_name+'/src/'+'requirements.txt'))
+
+    if not check_model_state:
+        return '* * * * Failed download src * * * * ' 
+    else:
+        return '* * * * Succeeded download src * * * * \n'+ str(check_model_state) 
+     
+def download(target):
+    x=True
+    # model_path='./'+ os.environ['MODEL_NAME']
+    
+    x = download_from_azure(target)
+    # sftp
+    if not x:
+        logger.info(" * * * Downloaded from sftp * * *")
+        x= download_from_sftp(target)
+    else:
+        logger.info(" * * * Downloaded from Azure * * * ")
+    
+    return x
+
+        #logger.info(" * * * Already model file exists * * * ")
+    
+    
+
+def download_from_azure(target):
+
+
+    name_list=[]
+
+    # model_name=os.getenv('MODEL_NAME')
+
+    try:
+        connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+        container_name = "edu"
+        container_client = blob_service_client.get_container_client(container_name)
+        
+        # path in azure
+
+        pwd=os.getcwd()
+        if target=='src':
+            blob_path = "/ysh_databricks/"+model_name+'/src/'
+            local_path_parent_folder=os.path.join(pwd,model_name,'src')
+        else:
+            blob_path = "/ysh_databricks/"+model_name+'/model/'
+            local_path_parent_folder=os.path.join(pwd,model_name,'model')
+
+        if not os.path.exists(local_path_parent_folder):
+            os.makedirs(local_path_parent_folder)
+
+
+        blob_list  = container_client.list_blobs(blob_path)
+       
+
+        if target=='src':
+            blob_path = blob_path+model_name+'.zip'
+            local_path_file= os.path.join(model_name,'src',model_name)+'.zip'  
+
+            logger.info(blob_path)
+            logger.info(local_path_file)
+
+        else:
+
+            for blob in blob_list:
+                print(blob)
+
+                _, tail = os.path.split("{}".format(blob.name))
+                name_list.append(int(tail[:8]))
+            
+            today=str(sorted(name_list)[-1])
+
+            
+            blob_path =blob_path+today+'.zip'
+            local_path_file= os.path.join(model_name,'model',today)+'.zip'  
+
+
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_path)
+
+
+
+        with open(local_path_file, "wb") as my_blob:
+            download_stream = blob_client.download_blob()
+            my_blob.write(download_stream.readall())
+        
+        
+        with zipfile.ZipFile(local_path_file, 'r') as zip_ref:
+            zip_ref.extractall(local_path_parent_folder)
+                
+        
+    except Exception as ex:
+        print(ex)
+        logger.info(ex)
+
+        return False
+
+    print("에져 다운로드 성공")
+    
+    return True
+
+
+
+
+def download_from_sftp(target):
+
+    
+    myHostname = os.environ['MY_HOST_NAME']
+    myUsername = os.environ['MY_USER_NAME']
+    myPassword = os.environ['MY_PASSWORD']
+
+    
+    '''
+    myHostname = "169.56.76.27"
+    myUsername = "d0106"
+    myPassword = "d0106!@#$"
+    '''
+    try: 
+        with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
+
+            print ("Connection succesfully stablished ... ")
+
+            if target=='src':
+                remoteFilePath = '/home/d0106/'+model_name+'/src/'+model_name+'.zip'
+                localFilePath = './'+model_name+'/src/'+model_name+'.zip'
+            else:
+                remoteFilePath = '/home/d0106/'+model_name+'/model/'
+                name_list = sftp.listdir(remoteFilePath)
+                
+                today=sorted([int(i[0:8]) for i in name_list])[-1]
+                
+                remoteFilePath= '/home/d0106/'+model_name+'/model/'+str(today)+'.zip'
+                localFilePath = './'+model_name+'/model/'+str(today)+'.zip'
+
+
+
+            sftp.get(remoteFilePath, localFilePath)
+            print(remoteFilePath,localFilePath)
+            #sftp.get_d(remoteFilePath, localFilePath, preserve_mtime=True)
+
+
+            if target=='src':
+                target_dir='./'+model_name+'/src/'
+            else:
+                target_dir='./'+model_name+'/model/'
+
+            with zipfile.ZipFile(localFilePath, 'r') as zip_ref:
+                zip_ref.extractall(target_dir)
+
+
+            
+    except Exception as ex:
+        print('-----------------------',ex)
+        logger.info(ex)
+        return False
+
+    return True
+
+def model_load():
+    '''
+    model_type=os.getenv('MODEL_TYPE')
+    model_name=os.getenv('MODEL_NAME')
+    '''
+    global tf_sess
+    os.system('pip3 install -r {}'.format(model_name+'/src/'+'requirements.txt'))
+    
+    try:
+        target_model=None
+
+        # scikit learn
+        if model_type =='sklearn':
+            import sklearn
+            import joblib
+            
+            model_file=os.listdir(os.path.join(model_name,'model'))
+            model_file=[i for i in model_file if i.endswith('.pkl')][0]
+            target_model= joblib.load('./'+model_name+'/model/'+model_file)
+        # tf model
+        else:
+            from k8s_api_model import k8s_api_model
+            import tensorflow as tf
+            target_model = k8s_api_model()
+            if 'build' in methods(k8s_api_model):
+                target_model.build()
+            # if build -> build / x -> x
+            try:
+                tf_sess = target_model.load('./'+model_name+'/model')
+            except Exception as ex:
+                logger.info(ex)
+    
+    except Exception as ex:
+        logger.info(ex)
+        return False
+
+    logger.info(' * * * * Succeed load model * * * *')    
+    return target_model
+            
+def methods(cls):
+    from types import FunctionType
+    return [x for x, y in cls.__dict__.items() if type(y) == FunctionType]
+
 
 if __name__ == '__main__':
+
+
     app.run(host='0.0.0.0', debug=True)
 
-# docker exec -it flask_sora /bin/bash -c "sh start_celery.sh"
-# /data01/logs/analy/maipksap01.allng.com/batch/ir_get_item_parameters_drl.log
+
